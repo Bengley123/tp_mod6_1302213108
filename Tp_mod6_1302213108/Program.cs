71c84825b67844cdf261ba287b9f6582c5440e9b
Bangley123 <yesasihombing1@gmail.com>
2023-03-27 00:41:10 +0700

    Add project files.

---
+++ Tp_mod6_1302213108/Program.cs
@@ -0,0 +1,46 @@
+﻿using tp_mod6_1302213108;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Panggil object dari class SayaTubeVideo yang menguji prekondisi dan exception
+        try
+        {
+            // membuat video dengan judul lebih dari 100 karakter (prekondisi tidak terpenuhi)
+            string videoTitle = "Tutorial Design By Contract - Implementasi Design by Contract pada OOP dengan C#";
+            SayaTubeVideo myVideo = new SayaTubeVideo(videoTitle);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        // membuat video dengan judul yang valid
+        string videoTitle2 = "Tutorial Design By Contract - Test Prekondisi & Exception";
+        SayaTubeVideo myVideo2 = new SayaTubeVideo(videoTitle2);
+
+        // panggil IncreasePlayCount dengan nilai yang valid
+        myVideo2.IncreasePlayCount(1000);
+
+        // panggil IncreasePlayCount dengan nilai yang melebihi batas maksimum (exception terjadi)
+        for (int i = 0; i < 20000000; i += 10000000)
+        {
+            try
+            {
+                myVideo2.IncreasePlayCount(10000000);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                break;
+            }
+        }
+
+        // cetak informasi detail video
+        myVideo2.PrintVideoDetails();
+
+        Console.ReadLine(); // keep console window open
+    }
+}
---
+++ Tp_mod6_1302213108/SayaTubeVideo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_mod6_1302213108
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string videoTitle)
+        {
+            // Prekondisi: Judul video memiliki panjang maksimal 100 karakter dan tidak berupa null.
+            if (videoTitle == null || videoTitle.Length > 100)
+            {
+                throw new ArgumentException("Judul video harus berisi antara 1-100 karakter");
+            }
+
+            // generate random id with 5 digits
+            Random rand = new Random();
+            id = rand.Next(10000, 99999);
+
+            title = videoTitle;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            // Prekondisi: Input penambahan play count maksimal 10.000.000 per panggilan method-nya.
+            if (count < 0 || count > 10000000)
+            {
+                throw new ArgumentOutOfRangeException("Jumlah penambahan play count harus di antara 0-10.000.000");
+            }
+
+            try
+            {
+                // pastikan tidak terjadi overflow pada penambahan playCount
+                playCount = checked(playCount + count);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("========== Video Details=========");
+            Console.WriteLine("ID           : " + id);
+            Console.WriteLine("Title        : " + title);
+            Console.WriteLine("Play Count   : " + playCount);
+        }
+    }
+}
